6dad6dbe658613ac949033311e50e857a75c8554
sam-eah <s.sam25@laposte.net>
2020-03-07 20:27:29 +0100

    first commit

---
+++ src/webparts/agentApp/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IAgentAppWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'AgentAppWebPartStrings' {
+  const strings: IAgentAppWebPartStrings;
+  export = strings;
+}
